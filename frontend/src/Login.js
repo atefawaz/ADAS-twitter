48c217bae4e42930308a2187075bb1852b359f58
atef fawaz <atefawaz@gmail.com>
2024-02-10 17:00:53 +0100

    added pic

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -39,22 +39,32 @@ function Login (){
     }
 
     return (
-        <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-            <div className="bg-white p-3 rounded w-25">
-            <h2>Login Form</h2>
+        <div className="d-flex justify-content-center align-items-center bg-black vh-100">
+            {/* Left Side - Picture */}
+            <div className="left-panel">
+                <img
+                    src="https://pbs.twimg.com/profile_images/1683498543967879173/EHRSRyrp_400x400.jpg"  
+                    alt="Login Background"
+                    className="img-fluid"
+                />
+            </div>
+
+            {/*  Login Form */}
+            <div className="right-panel bg-white p-3 rounded w-25">
+                <h2>Login Form</h2>
                 <form action="" onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="email"><strong>Email</strong></label>
-                        <input type="email" name="email" placeholder="Enter Your Email" onChange={handleInput} className="form-control rounded-0"></input>
+                        <input type="email" name="email" placeholder="Enter Your Email" onChange={handleInput} className="form-control rounded-0" />
                         {errors.email && <span className="text-danger">{errors.email}</span>}
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password"><strong>Password</strong></label>
-                        <input type="password" name="password" placeholder="Enter Your Password" onChange={handleInput} className="form-control rounded-0"></input>
+                        <input type="password" name="password" placeholder="Enter Your Password" onChange={handleInput} className="form-control rounded-0" />
                         {errors.password && <span className="text-danger">{errors.password}</span>}
                     </div>
                     <button type="submit" className="btn btn-success w-100">Login</button>
-                    <p>You are agree to our term and policies</p>
+                    <p>You agree to our terms and policies</p>
                     <Link to="/signup" className="btn btn-default border w-100 bg-light">Create Account</Link>
                 </form>
             </div>
