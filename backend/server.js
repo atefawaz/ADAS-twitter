2fd72ef28b67d5504bb4cae4018bda3aa2b95204
atef fawaz <atefawaz@gmail.com>
2024-02-12 20:47:48 +0100

    sever-changed-to-postgree

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,63 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
 
-const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'manager',
-    database : 'LoginRegister'
-})
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.post("/signup",(req,res)=>{
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password
-    ]
-
-    const query = "insert into login (`name`,`email`,`password`) values (?)"
-
-    db.query(query,[values],(err,data)=>{
-        // console.log(data)
-        if(err){
-            return res.json(err)
-        }
-        return res.json(data)
-    })
-})
+const db = new Pool({
+    user: 'ateffawaz',
+    host: 'localhost',
+    database: 'twitter-clone',
+    password: 'atef2004',
+    port: 5550,
+});
 
 
-app.post("/login",(req,res)=>{
-    const query = "select * from login where `email` = ? and `password` = ?"
+db.on('connect', () => {
+    console.log('Connected to PostgreSQL');
+});
 
-    db.query(query,[req.body.email,req.body.password],(err,data) =>{
-        // console.log(data)
-        // console.log(data.length)
-        if(err){
-            console.log(err)
-        }
-        if(data.length > 0){
-            return res.json("Success")
-        }
-        else{
-            return res.json("Fail")
-        }
-    })
-})
+db.on('end', () => {
+    console.log('Disconnected from PostgreSQL');
+});
 
 
-app.listen(2023,()=>{
-    console.log("server listening on 2023")
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+// app.post("/signup", (req, res) => {
+//     const values = [
+//         req.body.name,
+//         req.body.email,
+//         req.body.password
+//     ];
+
+//     const query = "INSERT INTO login (name, email, password) VALUES ($1, $2, $3)";
+
+//     db.query(query, values, (err, data) => {
+//         if (err) {
+//             return res.json(err);
+//         }
+//         return res.json(data);
+//     });
+// });
+
+// app.post("/login", (req, res) => {
+//     const query = "SELECT * FROM login WHERE email = $1 AND password = $2";
+
+//     db.query(query, [req.body.email, req.body.password], (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         if (data.rows.length > 0) {
+//             return res.json("Success");
+//         } else {
+//             return res.json("Fail");
+//         }
+//     });
+// });
+
+app.listen(2023, () => {
+    console.log("server listening on 2023");
+});
